701a2c9552d63cec093e45ef94a4f8926df13357
Masakazu Takahashi <55798814+MAA39@users.noreply.github.com>
2021-08-07 20:10:40 +0900

    Add task

---
+++ src/sample/03_Destructuring_assignment.js
@@ -0,0 +1,14 @@
+/**
+ * 分割代入
+ */
+
+const myProfile = {
+  name: "田中",
+  age: 19
+};
+
+const message = `名前は${myProfile.name}、年齢は${myProfile.age}歳です。`;
+console.log(message);
+
+//分割代入　＝　オブジェクトから指定のプロパティを抜き出して自由に使えるようになるもの
+//課題：
